7cc7822a3d5c8d387093432264aba1339895ef5b
Pushpak kumar <sonu7838787941@gmail.com>
2025-01-04 17:32:45 +0530

    fix

--- app/page.tsx
+++ app/page.tsx
@@ -2,7 +2,7 @@ import HeroCarousel from "@/components/HeroCarousel"
 import Searchbar from "@/components/Searchbar"
 import Image from "next/image"
 import { getAllProducts } from "@/lib/actions"
-import TrendingProduct from "@/components/TrendingProduct"
+import ProductCard from "@/components/ProductCard"
 
 const Home = async()=> {
   const allProducts = await getAllProducts();
@@ -39,7 +39,11 @@ const Home = async()=> {
       <section className="trending-section">
         <h2 className="section-text">Trending</h2>
 
-        <TrendingProduct/>
+        <div className="flex flex-wrap gap-x-8 gap-y-16">
+          {allProducts?.sort(() => 0.5 - Math.random()).slice(0, 8).map((product) => (
+              <ProductCard key={product._id} product={product}/>
+          ))}
+        </div>
       </section>
     </>
   )
